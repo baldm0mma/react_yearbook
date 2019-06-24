3f0c8edb037241ea935fc8f3be6d0341826d0d90
Jev Forsberg <jev.forsberg@gmail.com>
2019-06-24 17:37:08 -0600

    Iteration 2 Complete

--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -2,6 +2,19 @@ import React from 'react';
 import Person from './Person';
 import './Cohort.css';
 
-// COHORT COMPONENT CODE GOES HERE
+const Cohort = props => {
+  const staffMembers = props.staff.map(member => {
+    return <Person key={member.id} name={member.name} quote={member.quote} superlative={member.superlative} photo={member.photo} />
+  });
+  
+  return (
+    <div>
+      <h2 className='staff_header'>Staff</h2>
+      <div className='allStaff'>
+        {staffMembers}
+      </div>
+    </div>
+  );
+}
 
 export default Cohort;
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import './Person.css';
 
-// PERSON COMPONENT CODE GOES HERE
+const Person = props => {
+  
+  return (
+    <div className='member_container'>
+      <div className='photo_container'>
+        <img className='photo' src={props.photo} alt='staff member' />
+      </div>
+      <div className='name'>
+        {props.name}
+      </div>
+      <div className='quote'>
+        "{props.quote}"
+      </div>
+      <div className='superlative'>
+        {props.superlative}
+      </div>
+    </div>
+  );
+}
 
 export default Person;
