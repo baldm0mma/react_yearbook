6462a2be56fcffb28737fe51f42ab0c36fd953dd
Jev Forsberg <jev.forsberg@gmail.com>
2019-06-26 18:02:34 -0600

    Add form

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Cohort from './Cohort';
 import people from '../data/yearbook-data.js';
+import FormInput from './FormInput';
 import './App.css';
 
 class App extends Component {
@@ -9,15 +10,21 @@ class App extends Component {
     this.state = {
       staff: people.staff,
       students: people.students
-    }
+    };
   }
+
+  addNewHuman = newHuman => {
+    const add = { ...newHuman, id: Date.now() };
+    this.setState({ [newHuman.status]: [...this.state[newHuman.status], add] });
+  };
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className='App'>
+        <header className='App-header'>
           <h1>Turing Yearbook</h1>
         </header>
+        <FormInput addNewHuman={this.addNewHuman} />
         <Cohort staff={this.state.staff} students={this.state.students} />
       </div>
     );
